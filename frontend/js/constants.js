a9da866c4c185aedda2a7e32bdb76d6bdb603d1c
sylviakrupp <sylviakrupp@gmail.com>
2022-03-18 18:42:16 +0200

    bg color

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0x3b7eDb34B0bc60C2B88FB6CCd0fa3443B61a4EDF"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Bananaz land is so happy to see you here  !";
+const welcome_h1 = "Bananaz land is so happy to finally meet you!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = "Once upon a time there was a magical island that lived in a little girl's head. The island was occupied by the most happiest and magical creatures. The girl thought that the island was long gone. Until recently… In the hardest of times the island was found…by that same girl, but it wasn't a dream anymore. It was as real as these words in front of you. The artist didn't only find that island, but brought it out for the world to see. The first creatures discovered were 'Just Bananaz'. Originally inspired by a teeny-tiny toy banana. Little did she know that this tiny banana was going to have 7000+ friends to chill with in the future. Just Bananaz is inspired by the artist’s everyday life and inspirations. Find out more about these magical but still casual 'Just Bananaz' from Instagram when the time is right. <a href='https://youtube.com/codestackr'  target='_blank' rel='noopener noreferrer'>Just Bananaz</a>  Instagram.";
+const welcome_p = "Once upon a time there was a magical island that lived in a little girl's head. The island was occupied by the most happiest and magical creatures. The girl thought that the island was long gone. Until recently… In the hardest of times the island was found…by that same girl, but it wasn't a dream anymore. It was as real as these words in front of you. The artist didn't only find that island, but brought it out for the world to see. The first creatures discovered were 'Just Bananaz'. Originally inspired by a teeny-tiny toy banana. Little did she know that this tiny banana was going to have 7000+ friends to chill with in the future.";
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
